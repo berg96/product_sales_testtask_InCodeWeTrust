ff0d9e8192e498309fe33bb8100da193844bb496
Artem Kulikov <chigar2010@yandex.ru>
2025-03-23 01:51:11 +0200

    Добавлен и реализован простой кеш запросов. Добавлен Dockerfile и docker-compose

---
+++ product_sales/cache.py
@@ -0,0 +1,27 @@
+import threading
+from datetime import datetime, timedelta
+from time import sleep
+
+cache = {}
+CACHE_TTL = timedelta(minutes=5)
+
+def get_from_cache(key):
+    if key in cache:
+        timestamp, value = cache[key]
+        cache[key] = (datetime.utcnow(), value)
+        return value
+    return None
+
+def set_to_cache(key, value):
+    cache[key] = (datetime.utcnow(), value)
+
+def clear_expired_cache():
+    while True:
+        sleep(CACHE_TTL.total_seconds())  # Ждем 5 минут
+        now = datetime.utcnow()
+        keys_to_delete = [key for key, (timestamp, _) in cache.items() if now - timestamp >= CACHE_TTL]
+        for key in keys_to_delete:
+            del cache[key]
+
+# Запускаем поток для очистки кэша
+threading.Thread(target=clear_expired_cache, daemon=True).start()
--- product_sales/routes/sales.py
+++ product_sales/routes/sales.py
@@ -4,6 +4,7 @@ from flask import Blueprint, request, jsonify
 from sqlalchemy import func
 
 from product_sales import db
+from product_sales.cache import get_from_cache, set_to_cache
 from product_sales.models import Sale, Product
 from product_sales.error_handlers import InvalidAPIUsage
 
@@ -27,12 +28,19 @@ def get_total_sales():
             'В запросе отсутствуют обязательные поля: '
             f'{", ".join(missing_keys)}'
         )
-    return jsonify({'total': db.session.query(func.sum(Sale.quantity)).filter(
+    start_date = data['start_date']
+    end_date = data['end_date']
+    cache_key = f'get_total_sales_{start_date}_{end_date}'
+    if cached_result := get_from_cache(cache_key):
+        return jsonify({'cached': True, 'total': cached_result})
+    total_sales = db.session.query(func.sum(Sale.quantity)).filter(
         Sale.date.between(
-            datetime.strptime(data['start_date'], '%d.%m.%Y').date(),
-            datetime.strptime(data['end_date'], '%d.%m.%Y').date()
+            datetime.strptime(start_date, '%d.%m.%Y').date(),
+            datetime.strptime(end_date, '%d.%m.%Y').date()
         )
-    ).scalar()})
+    ).scalar()
+    set_to_cache(cache_key, total_sales)
+    return jsonify({'cached': False, 'total': total_sales})
 
 @sales_bp.route('/top-products', methods=['GET'])
 def get_top_products():
@@ -50,26 +58,33 @@ def get_top_products():
             'В запросе отсутствуют обязательные поля: '
             f'{", ".join(missing_keys)}'
         )
-    return jsonify(
-        {'top_products': [
-            # Выводим сериализованный продукт и сумму продаж этого продукта
-            {
-                'product': product.to_dict(),
-                'total_sales': total
-            # Извлекаем из БД Продукт и Сумму продаж,
-            # сгруппированную по Продукту, отфильтрованную по параметрам
-            # и отсортированную по убыванию
-            } for product, total in db.session.query(
-                Product, func.sum(Sale.quantity).label('total')
-            )
-            .join(Sale, Product.id == Sale.product_id)
-            .filter(Sale.date.between(
-                datetime.strptime(data['start_date'], '%d.%m.%Y').date(),
-                datetime.strptime(data['end_date'], '%d.%m.%Y').date()
-            ))
-            .group_by(Product)
-            .order_by(func.sum(Sale.quantity).desc())
-            .limit(data['limit'])
-            .all()
-        ]}
-    )
+    start_date = data['start_date']
+    end_date = data['end_date']
+    limit = data['limit']
+    cache_key = f'get_top_products_{start_date}_{end_date}_{limit}'
+    if cached_result := get_from_cache(cache_key):
+        return jsonify({'cached': True, 'top_products': cached_result})
+    top_products = [
+        # Задаем список словарей из
+        # сериализованного продукта и сумму продаж этого продукта
+        {
+            'product': product.to_dict(),
+            'total_sales': total
+        # Извлекаем из БД Продукт и Сумму продаж,
+        # сгруппированную по Продукту, отфильтрованную по параметрам
+        # и отсортированную по убыванию
+        } for product, total in db.session.query(
+            Product, func.sum(Sale.quantity).label('total')
+        )
+        .join(Sale, Product.id == Sale.product_id)
+        .filter(Sale.date.between(
+            datetime.strptime(start_date, '%d.%m.%Y').date(),
+            datetime.strptime(end_date, '%d.%m.%Y').date()
+        ))
+        .group_by(Product)
+        .order_by(func.sum(Sale.quantity).desc())
+        .limit(limit)
+        .all()
+    ]
+    set_to_cache(cache_key, top_products)
+    return jsonify({'cached': False, 'top_products': top_products})
